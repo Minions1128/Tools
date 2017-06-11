8d0e3eb73a4c87c591b48d586d76454673b8a492
Minions1128 <zhejian_shen@163.com>
2017-06-11 18:07:29 +0800

    Add files via upload

---
+++ test_mac.py
@@ -0,0 +1,19 @@
+import re
+
+def is_mac(mac):
+    if not mac or type(mac) not in [str, unicode]:
+        return False
+    re_str = ur'^([\da-f]{4})(\.([\da-f]){4}){2}$|^([\da-f]{2})([-.:]([\da-f]){2}){5}$'
+                      
+    ptn = re.compile(re_str, re.I)
+    if ptn.findall(mac):
+        return True
+    else:
+        return False
+
+
+print is_mac('AAaa.dddd.1111')
+print is_mac('4G-F0-2F-5C-08-A3')
+print is_mac('40-F0-2F-5C-08-A3')
+print is_mac('aa:33:dd:ff:DD:22')
+print is_mac('AA.aa.dd.dd.11.11')
